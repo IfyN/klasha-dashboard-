b2cdac6b559487cb4418b823cacb5295abceb22a
IfyN <ifynwosu0@gmail.com>
2023-04-14 17:11:43 +0100

    add styles and components to root folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+import Transactions from "./components/Transactions";
+import { useState } from "react";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={`entry ${showSidebar ? "open" : ""}`}>
+        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+        <Routes>
+          <Route path="/" Component={Dashboard} />
+          <Route path="/transactions" Component={Transactions} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
