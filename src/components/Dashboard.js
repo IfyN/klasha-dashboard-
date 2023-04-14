10cc542dadb6ac2c94cbd8724e3365793fcbcc83
IfyN <ifynwosu0@gmail.com>
2023-04-14 17:12:04 +0100

    dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import SalesView from "./SalesView";
+import { StyledDashboard } from "./Dashboard.styles";
+
+function Dashboard() {
+  return (
+    <SalesView />
+    // <StyledDashboard>
+    // </StyledDashboard>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/Dashboard.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledDashboard = styled.div`
+  display: grid;
+  width: inherit;
+  grid-auto-flow: column;
+  padding-top: 50px;
+`;
